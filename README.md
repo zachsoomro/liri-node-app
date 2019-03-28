4615172403cb0d574b7ad3af4074eca92b5cf8ee
Zach S <zachsoomro@gmail.com>
2019-03-27 23:41:47 -0500

    readme update

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # liri-node-app
+
+Used NPM init -y to install package
+Used NPM I to install Axios, Moment, DotEnv packages
+Created .gitignore to hide node_modules, .DS_Store, .env
+Linked key.js file to liri.js
--- liri.js
+++ liri.js
@@ -11,22 +11,15 @@ var moment = require("moment");
 var command = process.argv[2];
 var input = process.argv[3];
 
-// function bandsInTown() {
-//   var URL =
-//     "https://rest.bandsintown.com/artists/" + input + "/events?app_id=f8ac357c";
-
-//   request(URL, function(error, response, body) {
-//     if (error) console.log(error);
-//     var result = JSON.parse(body)[0];
-//     console.log(result);
-//   });
-// }
 var movieOutput = function() {
   request(
+    //insert user key
     "http://www.omdbapi.com/?i=tt3896198&apikey=f8ac357c=" + input,
     function(error, response, body) {
       var result = JSON.parse(body);
       console.log(result);
+      console.log("Actors: ", response.data);
+      console.log("Actors: ", response.data.Actors);
     }
   );
 };
@@ -40,3 +33,13 @@ function spotifyOutput(song) {
       console.log(response);
     });
 }
+// function bandsInTown() {
+//   var URL =
+//     "https://rest.bandsintown.com/artists/" + input + "/events?app_id=f8ac357c";
+
+//   request(URL, function(error, response, body) {
+//     if (error) console.log(error);
+//     var result = JSON.parse(body)[0];
+//     console.log(result);
+//   });
+// }
