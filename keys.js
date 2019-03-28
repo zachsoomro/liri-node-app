46e338b4748cd4779a34417073d173a66b860a32
Zach S <zachsoomro@gmail.com>
2019-03-27 22:24:01 -0500

    ombd

--- keys.js
+++ keys.js
@@ -1,6 +1,4 @@
-js;
 console.log("this is loaded");
-
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
--- liri.js
+++ liri.js
@@ -1,16 +1,32 @@
 require("dotenv").config();
 //required variables//
 var keys = require("./keys.js");
-var fs= require("fs");
-var request= require('request');
-var Spotify= require('node-spotify-api');
-var spotify= new Spotify(keys.spotify);
-var axios= require('axios');
-var moment= require('moment');
+var fs = require("fs");
+var request = require("request");
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+var axios = require("axios");
+var moment = require("moment");
 //user input variable
-var command= process.argv[2];
-var input= process.argv[3];
+var command = process.argv[2];
+var input = process.argv[3];
 
-function spotifyOutput = function(query){
-    spotify.search({type: 'track', query: query, limit:1})
-}
+// function bandsInTown() {
+//   var URL =
+//     "https://rest.bandsintown.com/artists/" + input + "/events?app_id=f8ac357c";
+
+//   request(URL, function(error, response, body) {
+//     if (error) console.log(error);
+//     var result = JSON.parse(body)[0];
+//     console.log(result);
+//   });
+// }
+var movieOutput = function() {
+  request(
+    "http://www.omdbapi.com/?i=tt3896198&apikey=f8ac357c=" + input,
+    function(error, response, body) {
+      var result = JSON.parse(body);
+      console.log(result);
+    }
+  );
+};
